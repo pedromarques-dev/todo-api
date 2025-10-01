c1f4f476d4ae2b1246c18a49a5b4d49e0ef8ffbf
Pedro Marques <phmarkes@hotmail.com>
2025-10-01 14:16:56 -0300

    add configp orject

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const port = process.env.PORT || 3333;
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .listen(port, ({ hostname, port }) =>
+    console.log(`🦊 Elysia is running at ${hostname}:${port}`)
+  );
+
